7cd08abf65476e646bd436cf552cb04934c96554
Wilson Prado <wilson.prado@superlogica.com>
2019-11-27 12:06:14 -0300

    first commit

--- index.js
+++ index.js
@@ -2,24 +2,55 @@
 
 const express = require('express');
 const axios = require('axios');
-const PORT = 5001;
+const PORT = 80;
 
 const app = express();
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 
 
 /** 
  * Função que busca as anomalias na API, grava no banco os ataques e retorna para o usuário;
 */
-app.get('/teste', (req, res) => {
+app.get('/infos', (req, res) => {
 
-    axios.get('https://api.hgbrasil.com/weather?key1008acee').then(function(data){
-      let retorno = data.data;
-      res.status(200).send({
-        status: 200,
-        infos: {
-          retorno
-        }
-      })
+  let retornoMocado = [{
+    id: 1,
+    idJogador: 1,
+    comando: "Avançar direita -> Avançar direita ->Avançar direita ->Avançar direita",
+  },
+  {
+    id: 2,
+    idJogador: 2,
+    comando: "Avançar esquerda -> Avançar esquerda -> Faça enquanto houver terreno",
+  },{
+    id: 3,
+    idJogador: 1,
+    comando: "Faça enquanto houver terreno",
+  },{
+    id: 4,
+    idJogador: 4,
+    comando: "Avançar direita -> Avançar direita ->Avançar direita ->Avançar direita",
+  },{
+    id: 5,
+    idJogador: 3,
+    comando: "Avançar direita",
+  },{
+    id: 6,
+    idJogador: 1,
+    comando: "Avançar direita -> Avançar esquerda",
+  }]
+
+    res.status(200).send({
+      status: 200,
+      infos: {
+        retornoMocado
+      }
     })
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,12 @@
 'use strict';
 
-const axios = require('axios');
-const url = '';
-
-var optionsPost = { method: 'POST',
-  url: 'https://upxtestebackend-3b72.restdb.io/rest/anomalies',
-  headers: 
-   { 'cache-control': 'no-cache',
-     'x-apikey': '412bfa50859d606423f0d54a5f02d19d72ddc',
-     'content-type': 'application/json' },
-  data: {},
-  json: true };
+let db = [];
 
 
-var optionsGet = { method: 'GET',
-  url: 'https://upxtestebackend-3b72.restdb.io/rest/anomalies',
-  headers: 
-  { 'cache-control': 'no-cache',
-      'x-apikey': '412bfa50859d606423f0d54a5f02d19d72ddc' } };
-
-
-module.exports.getAnomalies = () => {
-    let objToAxios = {
-        url: url,
-        method: 'GET'
-    };
-
-    return axios(objToAxios);
+module.exports.save = (data) => {
+    db.push(data);
 }
 
-
-module.exports.saveAnomalies = (anomalies) => {
-    optionsPost.data.anomalies = anomalies;
-    return axios(optionsPost);
-}
-
-module.exports.getDbAnomalies = () => {
-    return axios(optionsGet);
+module.exports.get = () => {
+    db.get(data);
 }
